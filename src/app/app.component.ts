50f0f5a46a3d1772ca465b5fd12b0c0461f0bf1e
redkjuegos <rasg.juegos@gmail.com>
2021-06-27 15:45:50 -0500

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo : string = 'Contador App';
+  numero : number = 0;
+  base : number = 5;
+
+  acumular( valor: number){
+  
+    this.numero += valor;
+
+  }
+
+
+}
+
